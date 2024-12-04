cf0904be6ffc50fe8e3441898aa76a3fe0d1b2c0
FanZejie <1227508134@qq.com>
2024-12-04 17:34:50 +0800

    temp commit for change pc

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,45 @@
-'use client';
+
 import Container from '@/common/components/elements/Container';
-import { NextSeo } from 'next-seo';
+import client from '@/common/libs/contentful/client';
 
-const PAGE_TITLE = 'Blog';
 
-const Blog = () => {
+interface BlogPost {
+    fields: {
+        title: string;
+        slug: string;
+        author: any;
+        excerpt: string;
+        coverImage: any;
+        content: any;
+    };
+    sys: {
+        id: string;
+    };
+    contentTypeId: string;
+}
+
+const Blog = async() => {
+  console.log(process.env.CONTENTFUL_ACCESS_TOKEN);
+  // 从 Contentful 获取博客文章
+  const { items } = await client.getEntries<BlogPost>({ content_type: 'blog' });
+
+  console.log(items);
     return (
         <>
-          <NextSeo title={`${PAGE_TITLE} - Ryan Aulia`} />
+      
           <Container className='xl:!-mt-5' data-aos='fade-up'>
             {/* <BlogListNew /> */}
-            <div>Blog</div>
+            <div>
+      <h1>博客文章</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.sys.id}>
+            <h2>{item.fields.title}</h2>
+            <p>{item.fields.excerpt}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
           </Container>
         </>
       );
