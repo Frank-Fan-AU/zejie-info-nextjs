5d78d276abf2c0ee8d781c3724bd62d0fafed876
Fanzejie <fanzejiea@gmail.com>
2024-12-10 14:18:05 +0800

    feat: finish dashboad,add some word explanation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,21 @@
+import Container from '@/common/components/elements/Container';
+import PageHeading from '@/common/components/elements/PageHeading';
+import Dashboard from './_components/Dashboard';
 import Breakline from '@/common/components/elements/Breakline';
-import { GITHUB_ACCOUNTS } from '@/common/constant/github';
-import Contributions from './_components/Contributions';
 
+const PAGE_TITLE = 'Dashboard';
+const PAGE_DESCRIPTION =
+  'This is my personal dashboard,Fetching data from Github API by GraphQL.';
 
-const Dashboard = () => {
+const DashboardPage = () => {
   return (
-    <>
-      <Breakline className='mb-8 mt-10' />
-      <div className='space-y-10'>
-        {GITHUB_ACCOUNTS?.filter((account) => account?.is_active).map(
-          (account, index) => (
-            <Contributions
-              key={index}
-              username={account?.username}
-              type={account?.type}
-              endpoint={account?.endpoint}
-            />
-          ),
-        )}
-      </div>
-    </>
+    <Container data-aos='fade-up'>
+    <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
+    <Dashboard />
+    <Breakline className='mb-8 mt-10' />
+    <p className='dark:text-neutral-400'>I have many other contributions beyond what is shown in the dashboard; however, some are in the company’s private GitLab repository, and others are private projects with code that cannot be made public.</p>
+  </Container>
   );
 };
 
-export default Dashboard;
+export default DashboardPage;
