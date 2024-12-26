f8996d26aa959d8b9a4b56b4692665d1ad93b57e
Fanzejie <fanzejiea@gmail.com>
2024-12-26 11:23:19 +0800

    fiex: mdx component no h1 before

--- src/common/components/elements/MDXComponent.tsx
+++ src/common/components/elements/MDXComponent.tsx
@@ -31,6 +31,12 @@ const MDXComponent = ({ children }: MarkdownRendererProps) => {
           />
         ),
         p: (props) => <div {...props} />,
+        h1: (props) => (
+          <h1
+            className='text-2xl font-medium dark:text-neutral-300'
+            {...props}
+          />
+        ),
         h2: (props) => (
           <h2
             className='text-xl font-medium dark:text-neutral-300'
